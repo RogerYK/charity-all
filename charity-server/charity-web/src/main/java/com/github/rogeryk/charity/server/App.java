34ce2dacfd5317251d715b7c82215bc69a5959ef
roger <rogeryk@outlook.com>
2019-08-06 09:02:51 +0800

    add docker

--- charity-server/charity-web/src/main/java/com/github/rogeryk/charity/server/App.java
+++ charity-server/charity-web/src/main/java/com/github/rogeryk/charity/server/App.java
@@ -1,20 +1,11 @@
 package com.github.rogeryk.charity.server;
 
 
-import com.github.rogeryk.charity.server.core.search.index.ProjectDocument;
-import com.github.rogeryk.charity.server.core.search.repository.ProjectDocumentRepository;
-import com.github.rogeryk.charity.server.db.domain.Project;
-import com.github.rogeryk.charity.server.db.repository.ProjectRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import javax.annotation.PostConstruct;
-
-import io.lettuce.core.dynamic.annotation.Command;
 
 @SpringBootApplication
 @EnableJpaAuditing
