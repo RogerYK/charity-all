26b6378667d6d750e39ff9f85d0b7b127475a2a4
roger <rogeryk@outlook.com>
2019-08-07 18:52:22 +0800

    Update show news watch count;

--- react-user/src/pages/NewsDetail/index.js
+++ react-user/src/pages/NewsDetail/index.js
@@ -1,64 +1,71 @@
-import React, { Component } from 'react'
-import { observable, action } from 'mobx';
-import { observer } from 'mobx-react';
-import styles from './style.module.scss'
-import api from '../../api';
-import { Avatar, Skeleton } from 'antd';
+import React, { Component } from "react";
+import { observable, action } from "mobx";
+import { observer } from "mobx-react";
+import styles from "./style.module.scss";
+import api from "../../api";
+import { Avatar, Skeleton } from "antd";
 
 @observer
 export default class NewsDetail extends Component {
+  @observable news;
 
-  @observable news
+  @observable pulling = true;
 
-  @observable pulling = true
+  @observable success = false;
 
-  @observable success = false
-
-  @observable msg
+  @observable msg;
 
   constructor(props) {
-    super(props)
-    console.log(props)
+    super(props);
+    console.log(props);
     api.News.detail(props.match.params.id)
-      .then(action(res => {
-        this.news = res.data
-        this.pulling = false
-        this.success = true
-      })).catch(res => {
-        this.msg = res.msg
-        this.pulling = false
-      })
+      .then(
+        action(res => {
+          this.news = res.data;
+          this.pulling = false;
+          this.success = true;
+        })
+      )
+      .catch(res => {
+        this.msg = res.msg;
+        this.pulling = false;
+      });
   }
 
   render() {
-    const {
-      news,
-      pulling,
-      success,
-      msg
-    } = this
+    const { news, pulling, success, msg } = this;
     return (
-      <div className={styles['main-wrap']}>
-        <div className={styles['main']}>
-        <Skeleton loading={pulling}>
-        {success ?
-          <>
-          <div className={styles['title']}>{news.title}</div>
-          <div className={styles['author']}>
-            <img className={styles['avatar']} src={news.author.avatar} alt="avatar"/>
-            <div className={styles['name']}>{news.author.nickName}</div>
-            <div className={styles['time']}>发布于{news.createdTime}</div>
-          </div>
-          <div className={styles['news-content']} dangerouslySetInnerHTML={{__html: news.content}}></div>
-          </>
-          :
-          <>
-          <div className={styles['error-msg']}>{msg}</div>
-          </>
-        }
-        </Skeleton>
+      <div className={styles["main-wrap"]}>
+        <div className={styles["main"]}>
+          <Skeleton loading={pulling}>
+            {success ? (
+              <>
+                <div className={styles["title"]}>{news.title}</div>
+                <div className={styles["author"]}>
+                  <img
+                    className={styles["avatar"]}
+                    src={news.author.avatar}
+                    alt="avatar"
+                  />
+                  <div className={styles["name"]}>{news.author.nickName}</div>
+                  <div className={styles["time"]}>发布于{news.createdTime}</div>
+                  <div className={styles["watch-count"]}>
+                    浏览人数 {news.watchCount}
+                  </div>
+                </div>
+                <div
+                  className={styles["news-content"]}
+                  dangerouslySetInnerHTML={{ __html: news.content }}
+                />
+              </>
+            ) : (
+              <>
+                <div className={styles["error-msg"]}>{msg}</div>
+              </>
+            )}
+          </Skeleton>
         </div>
       </div>
-    )
+    );
   }
 }
